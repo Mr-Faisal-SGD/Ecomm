1db91e1e28a1a61e2375dd813250c09894b4f3ab
Mr-Faisal-SGD <faisal450fai@gmail.com>
2023-09-05 06:16:13 -0700

    Add product page added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,7 @@ import { NavigationEnd, Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   menuType: string = 'buyer';
+  sellerName: string = '';
 
   constructor(private router: Router) {}
 
@@ -15,10 +16,21 @@ export class HeaderComponent implements OnInit {
     this.router.events.forEach((event) => {
       if (event instanceof NavigationEnd) {
         if (localStorage.getItem('seller') && event.url.includes('seller')) {
-          this.router.navigate(['/seller-home']);
+          // this.router.navigate(['/seller-home']);
           this.menuType = 'seller';
+          let sellerStore = localStorage.getItem('seller');
+          if (sellerStore) {
+            let seller = JSON.parse(sellerStore)[0];
+            this.sellerName = seller.name;
+          }
         } else this.menuType = 'buyer';
       }
     });
   }
+
+  logout() {
+    localStorage.removeItem('seller');
+    this.router.navigate(['/']);
+  }
+
 }
